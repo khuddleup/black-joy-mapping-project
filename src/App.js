102d5435a313606cdfdc381d93699854d17250d0
Sierra Parsons <heysierra@gmail.com>
2022-01-28 15:06:34 -0800

    added markers and updated icon to be a black fist

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import React from 'react';
 
+// import {ReactComponent as Fist} from "./fist.svg"
+
 import {
   GoogleMap,
   useLoadScript,
-  // Marker,
-  // InfoWindow,
+  Marker,
+  InfoWindow,
 } from "@react-google-maps/api";
 // import usePlacesAutocomplete, {
 //   getGeocode,
@@ -46,6 +48,26 @@ export default function App() {
     libraries,
   });
 
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+
+  const onMapClick = React.useCallback((e) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+
   if (loadError) return "Error";
   if (!isLoaded) return "Loading...";
 
@@ -56,7 +78,37 @@ export default function App() {
         mapContainerStyle={mapContainerStyle}
         zoom={11}
         center={center}
-      ></GoogleMap>
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {markers.map((marker) => (
+          <Marker
+            key={`${marker.lat}-${marker.lng}`}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+            icon={{
+              url: "https://upload.wikimedia.org/wikipedia/commons/e/e7/Fist_.svg",
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15),
+              scaledSize: new window.google.maps.Size(30, 30),
+            }}
+          />
+        ))}
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          > 
+          
+          </InfoWindow>
+
+        ) : null}
+
+        </GoogleMap>
     </div>
 
   );
